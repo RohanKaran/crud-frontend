6c30edbbebbecc7358c1e4e6fd905fb6220df16b
Rohan Karan <rohankaran001@gmail.com>
2021-12-14 01:55:51 +0530

    bad req removed

--- src/components/Update.js
+++ src/components/Update.js
@@ -86,7 +86,8 @@ function Update() {
 
 
                 <Button onClick={() => updateTodoHandler(nid)}
-                        href={'/crud-frontend'} type={'submit'}  className={" mx-2 mb-5"} variant={'success'}
+                        href={'/crud-frontend'}
+                        type={'submit'}  className={" mx-2 mb-5"} variant={'success'}
                                        style={{borderRadius: '50px', }}
 
                                        disabled={((title=== a ) && (desc === b)) || (title.trim() === "")}
